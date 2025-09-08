b19b7dd92b25e8151b01cb9ea97f2f334e71d79e
Shivam Kumar <shivamvats0987@gmail.com>
2025-09-08 10:03:37 +0530

    Added customers deafult route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,11 @@
+import { Navigate } from "react-router-dom"
 import { CustomerDetailsPage } from "../pages/CustomerDetailsPage";
 import { UnderConstruction } from "../pages/UnderConstruction";
 export const routes = [
+  {
+    path: "/",
+    element: <Navigate to="/customers" replace />,
+  },
   {
     path: "customers",
     element: <CustomerDetailsPage />,
